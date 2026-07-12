7c0d5c707d8a6ce9355aa042cda41d599f6e144f
naranjacc <naranjacc33@gmail.com>
2026-07-12 20:08:53 +0800

    Site updated: 2026-07-12 20:08:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/20/hello-world/","2026/06/20/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2026/06/21/cloudflare-pages-dns/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
