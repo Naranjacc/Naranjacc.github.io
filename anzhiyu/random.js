e9fa17eb59368afd8bc9450dba3cd478d0ef79e8
naranjacc <naranjacc33@gmail.com>
2026-07-29 17:13:16 +0800

    Site updated: 2026-07-29 17:13:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/21/cloudflare-pages-dns/"];function toRandomPost(){
+var posts=["2026/06/21/cloudflare-pages-dns/","2026/07/29/pcb-high-current-solder-mask-opening/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
